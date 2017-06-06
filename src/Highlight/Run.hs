83d79f3855def34135144d50d1e067ecad9a14ee
(cdep)illabout <cdep.illabout@gmail.com>
2017-06-06 12:36:47 +0900

    Make sure Japanese can be matched as well.

--- src/Highlight/Run.hs
+++ src/Highlight/Run.hs
@@ -1,8 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Highlight.Run where
 
-import Highlight.Options (Options(..))
+import qualified Data.ByteString as ByteString
+import Data.Text (pack)
+import Data.Text.Encoding (encodeUtf8)
+import Text.RE.PCRE ((*=~))
+import Text.RE.Replace (replaceAll)
+
+import Highlight.Options (Options(..), RegEx(unRegEx))
 
 run :: Options -> IO ()
-run opts = print opts
+run opts = do
+  let re = unRegEx $ optionsRegEx opts
+      lala = encodeUtf8 $ pack "this is a bytestring\nthin im 日本語 bytestring"
+      matches = lala *=~ re
+      repla = replaceAll "($0)" matches
+  ByteString.putStrLn lala
+  ByteString.putStrLn repla
 
