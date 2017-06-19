cfd37b3208b1ce71dc9bd1bba7dcf7f6fcd768f0
(cdep)illabout <cdep.illabout@gmail.com>
2017-06-19 21:01:36 +0900

    Fix up options code.

--- src/Highlight/Highlight/Options.hs
+++ src/Highlight/Highlight/Options.hs
@@ -4,30 +4,39 @@
 module Highlight.Highlight.Options
   ( ColorGrepFilenames(..)
   , colorGrepFilenamesParser
+  , HasColorGrepFilenames(..)
   , Options(..)
   , optionsParser
+  , HasOptions(..)
   , module Highlight.Common.Options
   ) where
 
 import Prelude ()
 import Prelude.Compat
 
-import Control.Applicative (many)
 import Data.Monoid ((<>))
-import Options.Applicative
-       (Parser, flag, help, long, metavar, short, strArgument)
+import Options.Applicative (Parser, flag, help, long, short)
 
 import Highlight.Common.Options
-       (CommonOptions, IgnoreCase, InputFilename(unInputFilename), RawRegex, Recursive(Recursive),
+       (CommonOptions, HasCommonOptions(getCommonOptions),
+        HasIgnoreCase(getIgnoreCase), HasInputFilenames(getInputFilenames),
+        HasRawRegex(getRawRegex), HasRecursive(getRecursive), IgnoreCase,
+        InputFilename(unInputFilename), RawRegex, Recursive(Recursive),
         commonOptionsParser)
+
+--------------------------
+-- Color grep filenames --
+--------------------------
 
 data ColorGrepFilenames = ColorGrepFilenames | DoNotColorGrepFileNames
   deriving (Eq, Read, Show)
 
-data Options = Options
-  { optionsColorGrepFilenames :: ColorGrepFilenames
-  , optionsCommonOptions :: CommonOptions
-  } deriving (Eq, Read, Show)
+class HasColorGrepFilenames r where
+  getColorGrepFilenames :: r -> ColorGrepFilenames
+
+instance HasColorGrepFilenames Options where
+  getColorGrepFilenames :: Options -> ColorGrepFilenames
+  getColorGrepFilenames = optionsColorGrepFilenames
 
 colorGrepFilenamesParser :: Parser ColorGrepFilenames
 colorGrepFilenamesParser =
@@ -36,6 +45,42 @@ colorGrepFilenamesParser =
         short 'g' <>
         help "highlight output from grep"
   in flag DoNotColorGrepFileNames ColorGrepFilenames mods
+
+-------------
+-- Options --
+-------------
+
+data Options = Options
+  { optionsColorGrepFilenames :: ColorGrepFilenames
+  , optionsCommonOptions :: CommonOptions
+  } deriving (Eq, Read, Show)
+
+class HasOptions r where
+  getOptions :: r -> Options
+
+instance HasOptions Options where
+  getOptions :: Options -> Options
+  getOptions = id
+
+instance HasIgnoreCase Options where
+  getIgnoreCase :: Options -> IgnoreCase
+  getIgnoreCase = getIgnoreCase . getCommonOptions
+
+instance HasRecursive Options where
+  getRecursive :: Options -> Recursive
+  getRecursive = getRecursive . getCommonOptions
+
+instance HasRawRegex Options where
+  getRawRegex :: Options -> RawRegex
+  getRawRegex = getRawRegex . getCommonOptions
+
+instance HasInputFilenames Options where
+  getInputFilenames :: Options -> [InputFilename]
+  getInputFilenames = getInputFilenames . getCommonOptions
+
+instance HasCommonOptions Options where
+  getCommonOptions :: Options -> CommonOptions
+  getCommonOptions = optionsCommonOptions
 
 optionsParser :: Parser Options
 optionsParser =
