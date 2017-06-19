39b60b1be4e4aaf1782a769876a1b6c2b0931547
(cdep)illabout <cdep.illabout@gmail.com>
2017-06-19 14:26:53 +0900

    Add childOf function that throws an error when the directory cannot be opened.

--- src/Highlight/Pipes.hs
+++ src/Highlight/Pipes.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Highlight.Pipes where
 
 import Control.Exception (throwIO, try)
+import Control.Monad (when)
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import Data.ByteString (ByteString, hGetLine, hPutStr)
 import Foreign.C.Error (Errno(Errno), ePIPE)
@@ -12,7 +14,15 @@ import GHC.IO.Exception
         ioe_type)
 import Pipes (Consumer', Producer, await, each, yield)
 import qualified Pipes.Prelude as Pipes
+import Pipes.Safe (MonadSafe, bracket)
+import System.FilePath ((</>))
 import System.IO (Handle, stderr)
+
+#ifdef mingw32_HOST_OS
+#else
+import System.Posix.Directory
+       (DirStream, closeDirStream, openDirStream, readDirStream)
+#endif
 
 import Highlight.Util (closeHandleIfEOFOrThrow)
 
@@ -45,3 +55,44 @@ stderrConsumer = go
         Left  e  -> liftIO $ throwIO e
         Right () -> go
 {-# INLINABLE stderrConsumer #-}
+
+
+-- | Select all immediate children of the given directory, ignoring @\".\"@ and
+-- @\"..\"@.
+--
+-- Throws an 'IOException' if the directory is not readable or (on Windows) if
+-- the directory is actually a reparse point.
+childOf :: MonadSafe m => FilePath -> Producer FilePath m ()
+childOf path = do
+#ifdef mingw32_HOST_OS
+  -- reparse <- liftIO $ fmap reparsePoint $ Win32.getFileAttributes path
+  -- when (canRead && not reparse) $
+  --     bracket
+  --         (liftIO $ Win32.findFirstFile (F.encodeString (path </> "*")))
+  --         (\(h, _) -> liftIO $ Win32.findClose h)
+  --         $ \(h, fdat) -> do
+  --             let loop = do
+  --                     file' <- liftIO $ Win32.getFindDataFileName fdat
+  --                     let file = F.decodeString file'
+  --                     when (file' /= "." && file' /= "..") $
+  --                         yield (path </> file)
+  --                     more  <- liftIO $ Win32.findNextFile h fdat
+  --                     when more loop
+  --             loop
+	error "Not implemented yet on Windows. Please send PR."
+#else
+  bracket (liftIO $ openDirStream path) (liftIO . closeDirStream) go
+  where
+    go :: MonadIO m => DirStream -> Producer FilePath m ()
+    go dirp = loop
+      where
+        loop :: MonadIO m => Producer FilePath m ()
+        loop = do
+          file <- liftIO $ readDirStream dirp
+          case file of
+            [] -> pure ()
+            _  -> do
+              when (file /= "." && file /= "..") . yield $ path </> file
+              loop
+#endif
+{-# INLINABLE childOf #-}
