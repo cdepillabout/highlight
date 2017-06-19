1e781b51ce84dd6a806fd0b5d70dce0d5e19950a
(cdep)illabout <cdep.illabout@gmail.com>
2017-06-19 21:34:51 +0900

    Get Hrep.Monad module working.

--- src/Highlight/Hrep/Monad.hs
+++ src/Highlight/Hrep/Monad.hs
@@ -28,189 +28,60 @@ import Pipes.Safe (runSafeT)
 import System.IO (stdin)
 
 import Highlight.Common.Error (HighlightErr(..))
+import Highlight.Common.Monad
+       (CommonHighlightM, FilenameHandlingFromFiles,
+        FileOrigin(FileFoundRecursively, FileSpecifiedByUser),
+        FileProducer, computeFilenameHandlingFromFiles,
+        getFilePathFromFileOrigin, getInputFilenamesM, getRecursiveM,
+        produerForSingleFile, runCommonHighlightM)
 import Highlight.Common.Options
-       (ColorGrepFilenames(ColorGrepFilenames, DoNotColorGrepFileNames),
-        IgnoreCase, InputFilename(unInputFilename), Options(..), RawRegex,
-        Recursive(Recursive))
+       (CommonOptions, IgnoreCase, InputFilename(unInputFilename),
+        RawRegex, Recursive(Recursive))
 import Highlight.Common.Pipes
        (childOf, fromHandleLines, numberedProducer, stderrConsumer)
 import Highlight.Common.Util
        (combineApplicatives, convertStringToRawByteString,
         openFilePathForReading)
 
-data FromGrepFilenameState = FromGrepFilenameState
-  { fromGrepFilenameStatePrevFileNum :: Int
-  , fromGrepFilenameStatePrevFilename :: (Maybe ByteString)
-  }
-
-initFromGrepFilenameState :: FromGrepFilenameState
-initFromGrepFilenameState =
-  FromGrepFilenameState
-  { fromGrepFilenameStatePrevFileNum = (-1)
-  , fromGrepFilenameStatePrevFilename = Nothing
-  }
-
-updateFilename :: MonadState FromGrepFilenameState m => ByteString -> m Int
-updateFilename nextFilename = do
-  FromGrepFilenameState prevFileNum prevFilename <- get
-  let justNextFilename = Just nextFilename
-  if justNextFilename == prevFilename
-    then return prevFileNum
-    else do
-      let nextFileNum = prevFileNum + 1
-      put $ FromGrepFilenameState nextFileNum justNextFilename
-      return nextFileNum
-
 -------------------------
 -- The Highlight Monad --
 -------------------------
 
-newtype HighlightM a = HighlightM
-  { unHighlightM
-      :: ReaderT
-          Options
-          (StateT FromGrepFilenameState (ExceptT HighlightErr IO))
-          a
-  } deriving ( Functor
-             , Applicative
-             , Monad
-             , MonadError HighlightErr
-             , MonadIO
-             , MonadReader Options
-             , MonadState FromGrepFilenameState
-             )
+type HighlightM = CommonHighlightM CommonOptions () HighlightErr
 
-runHighlightM :: Options -> HighlightM a -> IO (Either HighlightErr a)
-runHighlightM opts =
-  runExceptT .
-    flip evalStateT initFromGrepFilenameState .
-    flip runReaderT opts .
-    unHighlightM
-
-----------------------------------
--- Get value of certain options --
-----------------------------------
-
-getOptions :: HighlightM Options
-getOptions = ask
-
-getIgnoreCase :: HighlightM IgnoreCase
-getIgnoreCase  = reader optionsIgnoreCase
-
-getRecursive :: HighlightM Recursive
-getRecursive = reader optionsRecursive
-
-getColorGrepFilenames :: HighlightM ColorGrepFilenames
-getColorGrepFilenames = reader optionsColorGrepFilenames
-
-getRawRegex :: HighlightM RawRegex
-getRawRegex = reader optionsRawRegex
-
-getInputFilenames :: HighlightM [InputFilename]
-getInputFilenames = reader optionsInputFilenames
-
-------------------
--- Throw Errors --
-------------------
-
-throwHighlightErr :: HighlightErr -> HighlightM a
-throwHighlightErr = throwError
-
-throwRegexCompileErr :: RawRegex -> HighlightM a
-throwRegexCompileErr = throwHighlightErr . HighlightRegexCompileErr
+runHighlightM :: CommonOptions -> HighlightM a -> IO (Either HighlightErr a)
+runHighlightM opts = runCommonHighlightM opts ()
 
 -----------
 -- Pipes --
 -----------
 
-type Lala m a =
-  Producer
-    ( WhereDidFileComeFrom
-    , Either
-        (IOException, Maybe IOException)
-        (Producer ByteString m a)
-    )
-    m
-    a
-
-data WhereDidFileComeFrom
-  = FileSpecifiedByUser FilePath
-  | FileFoundRecursively FilePath
-  deriving (Eq, Read, Show)
-
-getFilePathFromWhereDid :: WhereDidFileComeFrom -> FilePath
-getFilePathFromWhereDid (FileSpecifiedByUser fp) = fp
-getFilePathFromWhereDid (FileFoundRecursively fp) = fp
-
 createInputData :: HighlightM (InputData HighlightM ())
 createInputData = do
   inputFilenames <-
-    fmap (FileSpecifiedByUser . unInputFilename) <$> getInputFilenames
-  recursive <- getRecursive
-  colorGrepFilenames <- getColorGrepFilenames
+    fmap (FileSpecifiedByUser . unInputFilename) <$> getInputFilenamesM
+  recursive <- getRecursiveM
   case inputFilenames of
     [] -> do
-      let filenameHandling = computeFilenameHandlingFromStdin colorGrepFilenames
-      return . InputDataStdin filenameHandling $ fromHandleLines stdin
+      return . InputDataStdin $ fromHandleLines stdin
     (file1:files) -> do
       let lalas =
             fmap
-              (producerForSingleFilePossiblyRecursive recursive)
+              (produerForSingleFile recursive)
               (file1 :| files)
-      let lala = foldl1 combineApplicatives lalas
-      (filenameHandling, newLala) <- computeFilenameHandlingFromFiles lala
-      return $ InputDataFile filenameHandling newLala
-
--- | TODO: It would be nice to turn this into two functions, one that just gets
--- a list of all files to read, and one that creates the 'Producer' that
--- actually pulles lines out of the file.
-producerForSingleFilePossiblyRecursive
-  :: forall m.
-     MonadIO m
-  => Recursive -> WhereDidFileComeFrom -> Lala m ()
-producerForSingleFilePossiblyRecursive recursive = go
-  where
-    go :: WhereDidFileComeFrom -> Lala m ()
-    go whereDid = do
-      let filePath = getFilePathFromWhereDid whereDid
-      eitherHandle <- openFilePathForReading filePath
-      case eitherHandle of
-        Right handle -> do
-          let linesProducer = fromHandleLines handle
-          yield (whereDid, Right linesProducer)
-        Left fileIOErr ->
-          if recursive == Recursive
-            then do
-              let producer = childOf filePath
-                  lIO = runSafeT $ toListM producer
-              eitherFileList <- liftIO (try lIO)
-              case eitherFileList of
-                Left dirIOErr -> do
-                  yield (whereDid, Left (fileIOErr, Just dirIOErr))
-                Right fileList -> do
-                  let sortedFileList = sort fileList
-                  let whereDids = fmap FileFoundRecursively sortedFileList
-                  let lalas =
-                        fmap
-                          (producerForSingleFilePossiblyRecursive recursive)
-                          whereDids
-                  for (each lalas) id
-            else
-              yield (whereDid, Left (fileIOErr, Nothing))
+      let fileProducer = foldl1 combineApplicatives lalas
+      (filenameHandling, newHighlightFileProducer) <-
+        computeFilenameHandlingFromFiles fileProducer
+      return $ InputDataFile filenameHandling newHighlightFileProducer
 
 data InputData m a
-  = InputDataStdin
-      FilenameHandlingFromStdin
-      (Producer ByteString m a)
+  = InputDataStdin (Producer ByteString m a)
   | InputDataFile
       FilenameHandlingFromFiles
-      (Lala m a)
+      (FileProducer m a)
 
 handleInputData
-  :: ( FilenameHandlingFromStdin
-        -> ByteString
-        -> HighlightM (NonEmpty ByteString)
-     )
+  :: (ByteString -> HighlightM (NonEmpty ByteString))
   -> ( FilenameHandlingFromFiles
         -> ByteString
         -> Int
@@ -224,22 +95,17 @@ handleInputData
       )
   -> InputData HighlightM ()
   -> HighlightM ()
-handleInputData stdinFunc _ _ (InputDataStdin filenameHandling producer) =
-  handleInputDataStdin stdinFunc filenameHandling producer
-handleInputData _ handleNonError handleError (InputDataFile filenameHandling lala) = do
-  handleInputDataFile handleNonError handleError filenameHandling lala
+handleInputData stdinFunc _ _ (InputDataStdin producer) =
+  handleInputDataStdin stdinFunc producer
+handleInputData _ handleNonError handleError (InputDataFile filenameHandling fileProducer) = do
+  handleInputDataFile handleNonError handleError filenameHandling fileProducer
 
 handleInputDataStdin
-  :: ( FilenameHandlingFromStdin
-        -> ByteString
-        -> HighlightM (NonEmpty ByteString)
-     )
-  -> FilenameHandlingFromStdin
+  :: (ByteString -> HighlightM (NonEmpty ByteString))
   -> Producer ByteString HighlightM ()
   -> HighlightM ()
-handleInputDataStdin f filenameHandling producer = do
-  runEffect $
-    producer >-> addNewline (f filenameHandling) >-> Pipes.ByteString.stdout
+handleInputDataStdin f producer = do
+  runEffect $ producer >-> addNewline f >-> Pipes.ByteString.stdout
   where
     addNewline
       :: forall m. Monad m
@@ -264,26 +130,26 @@ handleInputDataFile
      )
   -> (ByteString -> IOException -> Maybe IOException -> NonEmpty ByteString)
   -> FilenameHandlingFromFiles
-  -> Lala HighlightM ()
+  -> FileProducer HighlightM ()
   -> HighlightM ()
-handleInputDataFile handleNonError handleError filenameHandling lala = do
-  runEffect $ for (numberedProducer lala) g
+handleInputDataFile handleNonError handleError filenameHandling fileProducer = do
+  runEffect $ for (numberedProducer fileProducer) g
   where
     g
       :: ( Int
-         , WhereDidFileComeFrom
+         , FileOrigin
          , Either
             (IOException, Maybe IOException)
             (Producer ByteString HighlightM ())
          )
       -> Effect HighlightM ()
-    g (_, whereDid, Left (ioerr, maybeioerr)) = do
-      let filePath = getFilePathFromWhereDid whereDid
+    g (_, fileOrigin, Left (ioerr, maybeioerr)) = do
+      let filePath = getFilePathFromFileOrigin fileOrigin
       byteStringFilePath <- convertStringToRawByteString filePath
       let outputLines = handleError byteStringFilePath ioerr maybeioerr
       (each outputLines *> yield "\n") >-> stderrConsumer
-    g (fileNumber, whereDid, Right producer) = do
-      let filePath = getFilePathFromWhereDid whereDid
+    g (fileNumber, fileOrigin, Right producer) = do
+      let filePath = getFilePathFromFileOrigin fileOrigin
       byteStringFilePath <- convertStringToRawByteString filePath
       producer >-> bababa byteStringFilePath >-> Pipes.ByteString.stdout
       where
@@ -295,47 +161,3 @@ handleInputDataFile handleNonError handleError filenameHandling lala = do
           each outputLines
           yield "\n"
           bababa filePath
-
------------------------
--- Filename Handling --
------------------------
-
-data FilenameHandlingFromStdin
-  = FromStdinNoFilename
-  | FromStdinParseFilenameFromGrep
-  deriving (Eq, Read, Show)
-
-computeFilenameHandlingFromStdin
-  :: ColorGrepFilenames -> FilenameHandlingFromStdin
-computeFilenameHandlingFromStdin ColorGrepFilenames = FromStdinParseFilenameFromGrep
-computeFilenameHandlingFromStdin DoNotColorGrepFileNames = FromStdinNoFilename
-
-data FilenameHandlingFromFiles
-  = FromFilesNoFilename
-  | FromFilesPrintFilename
-  deriving (Eq, Read, Show)
-
-computeFilenameHandlingFromFiles
-  :: forall a m r.
-     Monad m
-  => Producer (WhereDidFileComeFrom, a) m r
-  -> m (FilenameHandlingFromFiles, Producer (WhereDidFileComeFrom, a) m r)
-computeFilenameHandlingFromFiles producer = do
-  eitherFirstFile <- next producer
-  case eitherFirstFile of
-    Left ret ->
-      return (FromFilesNoFilename, return ret)
-    Right ((whereDid1, a1), producer2) ->
-      case whereDid1 of
-        FileSpecifiedByUser _ -> do
-          eitherSecondFile <- next producer2
-          case eitherSecondFile of
-            Left ret2 ->
-              return (FromFilesNoFilename, yield (whereDid1, a1) *> return ret2)
-            Right ((whereDid2, a2), producer3) ->
-              return
-                ( FromFilesPrintFilename
-                , yield (whereDid1, a1) *> yield (whereDid2, a2) *> producer3
-                )
-        FileFoundRecursively _ ->
-          return (FromFilesPrintFilename, yield (whereDid1, a1) *> producer2)
